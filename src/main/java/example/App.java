c1e2a6b11bdcc4bcebc03a7727a2cf0db871ef62
yukihane <yukihane.feather@gmail.com>
2015-09-07 15:49:12 +0900

    print execute

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -1,13 +1,22 @@
 package example;
 
+import java.awt.print.PrinterException;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
 /**
  * Hello world!
- *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(final String[] args) throws PrinterException, IOException
     {
-        System.out.println( "Hello World!" );
+        System.out.println("file name: " + args[0]);
+        final File file = new File(args[0]);
+        try (final PDDocument doc = PDDocument.load(file)) {
+            doc.print();
+        }
     }
 }
